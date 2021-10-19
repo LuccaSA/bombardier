8852c65ad5a826d02a49326c64c33026c18777af
Luca Corrieri <lcorrieri@lucca.fr>
2021-10-19 14:55:14 +0200

    from v1.2.5: create go.mod file

--- build.py
+++ build.py
@@ -3,22 +3,7 @@ import os
 import subprocess
 
 platforms = [
-    ("darwin", "amd64"),
-    ("freebsd", "386"),
-    ("freebsd", "amd64"),
-    ("freebsd", "arm"),
-    ("linux", "386"),
     ("linux", "amd64"),
-    ("linux", "arm"),
-    ("linux", "arm64"),
-    ("netbsd", "386"),
-    ("netbsd", "amd64"),
-    ("netbsd", "arm"),
-    ("openbsd", "386"),
-    ("openbsd", "amd64"),
-    ("openbsd", "arm"),
-    ("openbsd", "arm64"),
-    ("windows", "386"),
     ("windows", "amd64"),
 ]
 
